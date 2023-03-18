d1293a86614b1189b1fb3d06d4a973fcb537af9c
chengze zheng <chengzez@bu.edu>
2023-03-18 09:29:13 -0400

    init repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data science analysis for environmental sustainability.',
+    author='Chengze, Arin, Zach, Alex',
+    license='MIT',
+)
